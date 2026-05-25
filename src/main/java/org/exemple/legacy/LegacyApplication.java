6bb2996d4d9eb88681ee1548812791e8d00e96d7
Titov Ilya <titov1608@gmail.com>
2026-05-25 10:28:30 +0300

    init db

--- src/main/java/org/exemple/legacy/LegacyApplication.java
+++ src/main/java/org/exemple/legacy/LegacyApplication.java
@@ -2,9 +2,7 @@ package org.exemple.legacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class LegacyApplication {
 
     public static void main(String[] args) {
